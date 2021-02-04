0e8bdf6f355cf693b973eb50bfeecb76af89024e
abhishek-shah-vst-au4 <54413214+abhishek-shah-vst-au4@users.noreply.github.com>
2021-02-04 22:46:23 +0530

    This is reactiveforms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'create-employee';
+
+  employeeForm!: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.employeeForm = new FormGroup({
+      fullName: new FormControl(),
+      email: new FormControl()
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.employeeForm.value);
+  }
 }
